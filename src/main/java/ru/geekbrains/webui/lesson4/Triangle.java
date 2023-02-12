8da73ec9966ef1a36305e354f65d33b7c78c3168
WhiteLight <qeros@yandex.ru>
2023-02-12 22:06:39 +0300

    lesson4

--- src/main/java/ru/geekbrains/webui/lesson4/Triangle.java
+++ src/main/java/ru/geekbrains/webui/lesson4/Triangle.java
@@ -3,7 +3,7 @@ package ru.geekbrains.webui.lesson4;
 public class Triangle {
 
     public long someMethod(int a, int b, int c) throws MyException {
-        if(a<0 || b<0) throw new MyException("Текст моей ошибки");
+        if(a<0 || b<0 || c<0) throw new MyException("Одно из значений меньше нуля");
         int p = (a+b+c)/2;
         double square = Math.sqrt((p*(p-a)*(p-b)*(p-c)));
         long result = Math.round(square);
