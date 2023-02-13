b76bc430b60ae13637371827a32651524fdd8871
WhiteLight <qeros@yandex.ru>
2023-02-14 00:16:23 +0300

    lesson5

--- src/test/java/ru/geekbrains/webui/lesson5/AppTest.java
+++ src/test/java/ru/geekbrains/webui/lesson5/AppTest.java
@@ -1,58 +1,82 @@
-package ru.geekbrains.webui.lesson3.test1;
+package ru.geekbrains.webui.lesson5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.sound.midi.Soundbank;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AppTest {
-    public static void main(String[] args) {
 
+    static WebDriver driver;
+
+    @BeforeAll
+    static void init(){
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         //options.addArguments("--headless");
         options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
 
-        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://www.livejournal.com");
+    }
 
-        //Тест 1
+    @BeforeEach
+    void getPage(){
+        driver.get("https://www.livejournal.com");
+    }
+
+    @Test
+    void test1() throws InterruptedException {
+
         WebElement webElement1 = driver.findElement(By.cssSelector(".s-header-item__link--login"));
         webElement1.click();
+        Thread.sleep(1000);
         WebElement webElement2 = driver.findElement(By.xpath(".//input[@id='user']"));
         webElement2.click();
         webElement2.sendKeys("UnioCornberry");
         WebElement webElement3 = driver.findElement(By.xpath(".//input[@id='lj_loginwidget_password']"));
         webElement3.click();
         webElement3.sendKeys("KJU1700dartsCOMP");
-        WebElement webElement4 = driver.findElement(By.xpath(".//button[@name='action:login']"));
+        webElement3.submit(); //вместо поиска кнопки войти и нажатия на нее, нажатие подтверждения
+        WebElement webElement4 = driver.findElement(By.cssSelector(".s-header-item--user"));
         webElement4.click();
+        Thread.sleep(2000);
+        Assertions.assertEquals("https://uniocornberry.livejournal.com/",driver.getCurrentUrl(),"Логин не произведен");
 
-        //Тест 2
+    }
+
+    @Test
+    void test2() throws InterruptedException {
+
         WebElement webElement5 = driver.findElement(By.cssSelector(".s-header-item--user"));
         webElement5.click();
         WebElement webElement6 = driver.findElement(By.cssSelector(".s-header-sub-list-item__link--settings"));
         webElement6.click();
+        Thread.sleep(5000);
         WebElement webElement7 = driver.findElement(By.xpath(".//a[contains(text(),'Редактировать профиль')]"));
         webElement7.click();
+        Thread.sleep(5000);
         WebElement webElement8 = driver.findElement(By.xpath(".//form[@action='https://www.livejournal.com/manage/profile/?authas=']"));
         webElement8.click();
+        Thread.sleep(5000);
         WebElement webElement9 = driver.findElement(By.xpath(".//textarea[@name='journal_title']"));
         webElement9.click();
         webElement9.sendKeys("Почти самый лучший журнал");
         WebElement webElement10 = driver.findElement(By.cssSelector(".b-standout > .b-flatbutton"));
         webElement10.click();
+        Thread.sleep(5000);
 
-        //Тест 3
+    }
+
+    @Test
+    void test3() throws InterruptedException {
+
         WebElement webElement11 = driver.findElement(By.cssSelector(".s-header-item-post--short"));
         webElement11.click();
         WebElement webElement12 = driver.findElement(By.cssSelector(".text-0-2-179"));
@@ -64,9 +88,11 @@ public class AppTest {
         webElement14.click();
         WebElement webElement15 = driver.findElement(By.cssSelector(".rootMaxWidth-0-2-99 > .rootIn-0-2-98"));
         webElement15.click();
+    }
 
-        //
-        //Тест 4
+    @Test
+    void test4() throws InterruptedException {
+
         WebElement webElement16 = driver.findElement(By.cssSelector(".s-header-item--user"));
         webElement16.click();
         WebElement webElement17 = driver.findElement(By.cssSelector(".s-header-sub-list-item__link--logout"));
@@ -74,4 +100,10 @@ public class AppTest {
 
     }
 
+
+
+    @AfterAll
+    static void close(){
+        driver.quit();
+    }
 }
